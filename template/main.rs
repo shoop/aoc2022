2684b4aa477291a8c0786b9e6b83231b18542dcd
Stijn Hoop <stijn@sandcat.nl>
2022-12-03 15:50:26 +0100

    Add my solution template main file

---
+++ template/main.rs
@@ -0,0 +1,46 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::vec::Vec;
+
+fn star_one(_lines: &Vec<String>) -> i32 {
+    0
+}
+
+fn star_two(_lines: &Vec<String>) -> i32 {
+    0
+}
+
+fn main() {
+    let file = File::open("./input").expect("Unreadable input file ./input");
+    let lines: Vec<String> = io::BufReader::new(file)
+        .lines()
+        .map(|x| x.expect("Could not read line"))
+        .collect();
+
+    let ans = star_one(&lines);
+    println!("Star one: {}", ans);
+
+    let ans = star_two(&lines);
+    println!("Star two: {}", ans);
+}
+
+#[cfg(test)]
+mod tests {
+    static TEST_DATA: &str = "";
+
+    #[test]
+    fn test_star_one() {
+        let lines: Vec<String> = TEST_DATA.lines().map(|x| x.to_string()).collect();
+
+        let ans = super::star_one(&lines);
+        assert_eq!(ans, 1);
+    }
+
+    #[test]
+    fn test_star_two() {
+        let lines: Vec<String> = TEST_DATA.lines().map(|x| x.to_string()).collect();
+
+        let ans = super::star_two(&lines);
+        assert_eq!(ans, 2);
+    }
+}
